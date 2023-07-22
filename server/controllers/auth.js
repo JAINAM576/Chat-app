dde6ccbc5faa2c944ac703c91c3f23ec37050cc7
JAINAM576 <het170246@gmail.com>
2023-07-22 22:16:54 +0530

    all set

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -11,7 +11,12 @@ const api_secret=process.env.STREAM_API_SECRET;
 const signup=async (req,res)=>{
 try {
     const {fullName,Username,PhoneNumber,AvatarURL,Password,ConfirmPassword}=req.body;
-
+    const client=StreamChat.getInstance(api_key,api_secret);
+    const {users}=await client.queryUsers({name:Username})
+    if(users.length){
+        return res.status(200).json({message:"U3"});
+        
+            }
   const userId=  crypto.randomBytes(16).toString('hex');
   const serverClient=connect(api_key,api_secret,api_id)
 const haseshpass=await bcrypt.hash(Password,10);
